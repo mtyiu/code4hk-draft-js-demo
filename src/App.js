0196ea243e04626d56cdf0b5d779cdeadc83e5c1
mtyiu <mattmtyiu@gmail.com>
2017-03-15 00:06:04 +0800

    Implement initialization with HTML

--- src/App.js
+++ src/App.js
@@ -4,23 +4,44 @@ import './App.css';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-import { convertToRaw } from 'draft-js';
+import { convertToRaw, EditorState, convertFromHTML, ContentState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+
+import fetch from 'isomorphic-fetch';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { html: '' };
+    this.state = {
+      html: '',
+      editorState: EditorState.createEmpty()
+    };
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
   }
 
+  componentDidMount() {
+    fetch('/sample.html')
+      .then(response => response.text())
+      .then(html => {
+        // Convert HTML to editor state
+        console.log(html);
+        const blocksFromHTML = convertFromHTML(html);
+        const state = ContentState.createFromBlockArray(
+          blocksFromHTML.contentBlocks,
+          blocksFromHTML.entityMap
+        );
+        return EditorState.createWithContent(state);
+      })
+      .then(editorState => this.setState({ editorState }));
+  }
+
   onEditorStateChange(editorState) {
-    this.setState({
-      html: draftToHtml( convertToRaw( editorState.getCurrentContent() ) )
-    });
+    this.setState({ editorState });
   }
 
   render() {
+    const html = draftToHtml( convertToRaw( this.state.editorState.getCurrentContent() ) );
+
     return (
       <div className="App">
         <div className="App-header">
@@ -31,6 +52,7 @@ class App extends Component {
             <div className="col m6">
               <div className="card-panel">
                 <Editor
+                  editorState={ this.state.editorState }
                   placeholder="Put your content here..."
                   onEditorStateChange={ this.onEditorStateChange }
                   />
@@ -39,7 +61,7 @@ class App extends Component {
             <div className="col m6">
               <div className="card-panel grey lighten-4">
                 <h5>HTML:</h5>
-                <pre>{ this.state.html }</pre>
+                <pre>{ html }</pre>
               </div>
             </div>
           </div>
